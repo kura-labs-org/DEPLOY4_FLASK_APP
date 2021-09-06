23f8c2572ec83856a2202a44a8deaeb640a30598
kawangwong <nycazn724@gmail.com>
2021-09-06 13:24:28 -0400

    Add files via upload
    Added more manual code. to get around local variable issues and fixed return statement issues on 500 error for return none

--- DogbiteApp/datagrabber.py
+++ DogbiteApp/datagrabber.py
@@ -8,7 +8,7 @@ prettyprint = formattedURLforJson.json()[1] #change the index number here.
 
 ###testing function for print
 def testerthing():
-  print("hello!")
+  print("herro!")
 
 
 def appearance(phrase):
@@ -23,99 +23,105 @@ def appearance(phrase):
       Male += 1
     if theGender == "M" and i == x-1:
       Male += 1
-      print(f"There are {Male} male dogs that bit people in 2015.")
-      print(f"There are {Female} female dogs that bit people in 2015.")
-      print(f"There are {Unknown} unidentified gender of dogs that bit people in 2015.")
+      statement1 =(f"There are {Male} male dogs that bit people in 2015.")
+      statement2 = (f"There are {Female} female dogs that bit people in 2015.")
+      statement3 = (f"There are {Unknown} unidentified gender of dogs that bit people in 2015.")
+      allstatements = (f"{statement1} \n {statement2} \n {statement3}")
+      return allstatements      
     if theGender == "F":
       Female += 1
     if theGender == "F" and i == x-1:
       Female += 1
-      print(f"There are {Male} male dogs that bit people in 2015.")
-      print(f"There are {Female} female dogs that bit people in 2015.")
-      print(f"There are {Unknown} unidentified gender of dogs that bit people in 2015.")
+      statement1 =(f"There are {Male} male dogs that bit people in 2015.")
+      statement2 = (f"There are {Female} female dogs that bit people in 2015.")
+      statement3 = (f"There are {Unknown} unidentified gender of dogs that bit people in 2015.")
+      allstatements = (f"{statement1} \n {statement2} \n {statement3}")
+      return allstatements
     if theGender == "U":
       Unknown += 1
     if theGender == "U" and i == x-1:
       Unknown += 1
-      print(f"There are {Male} male dogs that bit people in 2015.")
-      print(f"There are {Female} female dogs that bit people in 2015.")
-      print(f"There are {Unknown} unidentified gender of dogs that bit people in 2015.")
+      statement1 =(f"There are {Male} male dogs that bit people in 2015.")
+      statement2 = (f"There are {Female} female dogs that bit people in 2015.")
+      statement3 = (f"There are {Unknown} unidentified gender of dogs that bit people in 2015.")
+      allstatements = (f"{statement1} \n {statement2} \n {statement3}")
+      return allstatements
 
 appearance("gender")
 
-def location(spot):
-  Queens = 0
-  Bronx = 0
-  Brooklyn = 0
-  Manhattan = 0
-  Staten_Island = 0
-  Other = 0
-  x = 10 #change value for desired range
-  for i in range(x):
-    prettyprint = formattedURLforJson.json()[i]
-    theSpot = prettyprint.get(spot, "Other")
-    if theSpot == "Queens":
-      Queens += 1
-    if theSpot == "Queens" and i == x-1:
-      Queens += 1
-      print(Queens)
-      print(Bronx)
-      print(Brooklyn)
-      print(Manhattan)
-      print(Staten_Island)
-      print(Other)
-    if theSpot == "Bronx":
-      Bronx += 1
-    if theSpot == "Bronx" and i == x-1:
-      Bronx += 1
-      print(Queens)
-      print(Bronx)
-      print(Brooklyn)
-      print(Manhattan)
-      print(Staten_Island)
-      print(Other)
-    if theSpot == "Brooklyn":
-      Brooklyn += 1
-    if theSpot == "Brooklyn" and i == x-1:
-      Brooklyn += 1
-      print(Queens)
-      print(Bronx)
-      print(Brooklyn)
-      print(Manhattan)
-      print(Staten_Island)
-      print(Other)
-    if theSpot == "Manhattan":
-      Manhattan += 1
-    if theSpot == "Manhattan" and i == x-1:
-      Manhattan += 1
-      print(Queens)
-      print(Bronx)
-      print(Brooklyn)
-      print(Manhattan)
-      print(Staten_Island)
-      print(Other)
-    if theSpot == "Staten Island":
-      Staten_Island += 1
-    if theSpot == "Staten Island" and i == x-1:
-      Staten_Island += 1
-      print(Queens)
-      print(Bronx)
-      print(Brooklyn)
-      print(Manhattan)
-      print(Staten_Island)
-      print(Other)
-    if theSpot == "Other" and i == x-1:
-      Unknown += 1
-    if theSpot == "Other" and i == x-1:
-      Unknown += 1
-      print(Queens)
-      print(Bronx)
-      print(Brooklyn)
-      print(Manhattan)
-      print(Staten_Island)
-      print(Other)
+# def location(spot):
+#   Queens = 0
+#   Bronx = 0
+#   Brooklyn = 0
+#   Manhattan = 0
+#   Staten_Island = 0
+#   Other = 0
+#   x = 10 #change value for desired range
+#   for i in range(x):
+#     prettyprint = formattedURLforJson.json()[i]
+#     theSpot = prettyprint.get(spot, "Other")
+#     if theSpot == "Queens":
+#       Queens += 1
+#     if theSpot == "Queens" and i == x-1:
+#       Queens += 1
+#       print(Queens)
+#       print(Bronx)
+#       print(Brooklyn)
+#       print(Manhattan)
+#       print(Staten_Island)
+#       print(Other)
+#     if theSpot == "Bronx":
+#       Bronx += 1
+#     if theSpot == "Bronx" and i == x-1:
+#       Bronx += 1
+#       print(Queens)
+#       print(Bronx)
+#       print(Brooklyn)
+#       print(Manhattan)
+#       print(Staten_Island)
+#       print(Other)
+#     if theSpot == "Brooklyn":
+#       Brooklyn += 1
+#     if theSpot == "Brooklyn" and i == x-1:
+#       Brooklyn += 1
+#       print(Queens)
+#       print(Bronx)
+#       print(Brooklyn)
+#       print(Manhattan)
+#       print(Staten_Island)
+#       print(Other)
+#     if theSpot == "Manhattan":
+#       Manhattan += 1
+#     if theSpot == "Manhattan" and i == x-1:
+#       Manhattan += 1
+#       print(Queens)
+#       print(Bronx)
+#       print(Brooklyn)
+#       print(Manhattan)
+#       print(Staten_Island)
+#       print(Other)
+#     if theSpot == "Staten Island":
+#       Staten_Island += 1
+#     if theSpot == "Staten Island" and i == x-1:
+#       Staten_Island += 1
+#       print(Queens)
+#       print(Bronx)
+#       print(Brooklyn)
+#       print(Manhattan)
+#       print(Staten_Island)
+#       print(Other)
+#     if theSpot == "Other" and i == x-1:
+#       Unknown += 1
+#     if theSpot == "Other" and i == x-1:
+#       Unknown += 1
+#       print(Queens)
+#       print(Bronx)
+#       print(Brooklyn)
+#       print(Manhattan)
+#       print(Staten_Island)
+#       print(Other)
 
-location("borough")
+# location("borough")
 
 
 # prettyprint.get("breed", None)
