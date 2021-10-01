5867c76b406b4c9d9d7a94541aa038a0c00879d6
kura-labs01 <86966558+kura-labs01@users.noreply.github.com>
2021-10-01 19:03:06 -0400

    Update application.py

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ import json
 import os.path
 from werkzeug.utils import secure_filename
 
+# change to just application = Flask(__name__)
 application = app = Flask(__name__)
 app.secret_key = 'hfsdfhghergjhfg'
 
